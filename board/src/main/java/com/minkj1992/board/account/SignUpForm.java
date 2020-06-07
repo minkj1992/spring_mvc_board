51ddb6d5225c525bab8ecded6a7bd8bab04289de
minkj1992 <minkj1992@gmail.com>
2020-06-07 18:59:01 +0900

    feat: Update sign-up.html & Add SignUpForm to show login page

---
+++ board/src/main/java/com/minkj1992/board/account/SignUpForm.java
@@ -0,0 +1,14 @@
+package com.minkj1992.board.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+}
+
