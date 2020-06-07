278422e2862d439b3f3328b631c0622ce0cf244f
minkj1992 <minkj1992@gmail.com>
2020-06-07 19:17:08 +0900

    test: Update signup test code to Verify whether form exists

--- board/src/test/java/com/minkj1992/board/account/AccountControllerTest.java
+++ board/src/test/java/com/minkj1992/board/account/AccountControllerTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,7 +21,8 @@ class AccountControllerTest {
     public void signUpForm() throws Exception {
         mockMvc.perform(get("/sign-up"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("account/sign-up"));
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(model().attributeExists("signUpForm"));
     }
 
 }
