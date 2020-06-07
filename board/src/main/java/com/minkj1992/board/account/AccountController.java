0ded5af3ed63660ac923a8592420cb0dd04c317b
minkj1992 <minkj1992@gmail.com>
2020-06-07 20:51:18 +0900

    feat: Add custom sign-up validator and Apply to Controller with InitBinder

--- board/src/main/java/com/minkj1992/board/account/AccountController.java
+++ board/src/main/java/com/minkj1992/board/account/AccountController.java
@@ -1,15 +1,26 @@
 package com.minkj1992.board.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
 
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
