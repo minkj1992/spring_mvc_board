ab616a27cbc799fadd115a2178018da803a13478
minkj1992 <minkj1992@gmail.com>
2020-06-07 20:30:12 +0900

    feat: Add sign-up form validation

--- board/src/main/java/com/minkj1992/board/account/AccountController.java
+++ board/src/main/java/com/minkj1992/board/account/AccountController.java
@@ -2,7 +2,11 @@ package com.minkj1992.board.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.Valid;
+
 
 @Controller
 public class AccountController {
@@ -11,5 +15,16 @@ public class AccountController {
     public String signUpForm(Model model) {
         model.addAttribute(new SignUpForm()); // Attribute 이름이 비슷한 경우 attribute key에 해당하는 str 생략가능
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        // TODO 회원가입 처리
+        return "redirect:/";
+
     }
 }
