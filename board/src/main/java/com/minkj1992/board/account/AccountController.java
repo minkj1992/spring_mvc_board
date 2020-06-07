ee970332b0d8a19ec31d72cb149521dba812416d
minkj1992 <minkj1992@gmail.com>
2020-06-07 19:13:32 +0900

    refactor: Simplify account controller

--- board/src/main/java/com/minkj1992/board/account/AccountController.java
+++ board/src/main/java/com/minkj1992/board/account/AccountController.java
@@ -9,7 +9,7 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
-        model.addAttribute("signUpForm", new SignUpForm());
+        model.addAttribute(new SignUpForm()); // Attribute 이름이 비슷한 경우 attribute key에 해당하는 str 생략가능
         return "account/sign-up";
     }
 }
